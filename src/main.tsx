a527fab8e4d6d81834af0aa570d82e1b7a8ea76a
ricomuh <rico.smartlolo@gmail.com>
2024-10-21 16:08:18 +0700

    Add login page route to router configuration

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import Home from "./pages/page";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import LoginPage from "./pages/auth/login/page";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/auth/login",
+    element: <LoginPage />,
   },
 ]);
 
