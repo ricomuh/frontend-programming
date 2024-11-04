e57a233d797385877178f67131a9c947ffff2f23
ricomuh <rico.smartlolo@gmail.com>
2024-11-04 17:21:14 +0700

    Add search input and icon to Header component; update Input component to conditionally render label

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,5 @@
+import Input from "./input";
+
 export default function Header(): JSX.Element {
   return (
     <header className="border-b-2 p-6 flex items-center justify-between">
@@ -5,9 +7,26 @@ export default function Header(): JSX.Element {
         <div className="font-bold text-lg">Username</div>
         <div className="ms-6 text-gray-300">Oct 17, 2024</div>
       </div>
-      <div className="flex">
-        <div>Icon</div>
-        <div className="ms-10 hidden sm:block">Search Box</div>
+      <div className="flex items-center">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+            />
+          </svg>
+        </div>
+        <div className="ms-10 hidden sm:block">
+          <Input label="" id="search" type="search" placeholder="Search" />
+        </div>
       </div>
     </header>
   );
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -9,9 +9,11 @@ export default function Input({
 }): JSX.Element {
   return (
     <div>
-      <label htmlFor={id} className="block text-sm font-medium text-gray-700">
-        {label}
-      </label>
+      {label.length > 0 && (
+        <label htmlFor={id} className="block text-sm font-medium text-gray-700">
+          {label}
+        </label>
+      )}
       <input
         {...props}
         className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
